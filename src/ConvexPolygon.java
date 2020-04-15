fdb389c6dcc9d5e75b2edc92c47ab80fd6ba4a56
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-04-16 00:24:24 +0200

    Add initial classes and methods

---
+++ src/ConvexPolygon.java
@@ -0,0 +1,21 @@
+public class ConvexPolygon {
+    // checks if point p is inside of the convex polygon
+    public boolean containsPoint(Point p) {
+        return false;
+    }
+
+    // returns the i-th corner point of the convex polygon
+    public Point get(int i) {
+        return null;
+    }
+
+    // returns the number of corner points of the convex polygon
+    public int size() {
+        return 0;
+    }
+
+    // constructs a string in the format: [(x1,y1), ..., (xn,yn)].
+    public String toString() {
+        return "test";
+    }
+}
---
+++ src/TestConvexHull.java
@@ -0,0 +1,6 @@
+public class TestConvexHull {
+    public static void main(String[] args) {
+        PointSet set1 = new PointSet();
+        System.out.println(set1.toString());
+    }
+}
