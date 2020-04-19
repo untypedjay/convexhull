1411edf981366cf8ce06e3b131c8e33b2cfdba6d
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-04-19 23:37:33 +0200

    Implement capacity() and size())

--- src/PointSet.java
+++ src/PointSet.java
@@ -1,55 +1,58 @@
 public class PointSet {
-    // constructs a void set of points with capacity 0
+    private int capacity;
+    private int size;
+
+    // TODO constructs a void set of points with capacity 0
     public PointSet() {
 
     }
 
-    // constructs a void set of points with the given capacity
+    // TODO constructs a void set of points with the given capacity
     public PointSet(int capacity) {
 
     }
 
-    // constructs a set of points with the corner points of a convex polygon
+    // TODO constructs a set of points with the corner points of a convex polygon
     public PointSet(ConvexPolygon points) {
 
     }
 
     // returns the capacity of the set of points
     public int capacity() {
-        return 0;
+        return this.capacity;
     }
 
     // returns the size of the set of points
     public int size() {
-        return 0;
+        return this.size;
     }
 
-    // changes the capacity of the set of points (newCapacity can be greater, smaller or equal than the old capacity
+    // TODO changes the capacity of the set of points (newCapacity can be greater, smaller or equal than the old capacity
     public void changeCapacity(int newCapacity) {
 
     }
 
-    // adds a point to the set and increases the capacity if necessary
+    // TODO adds a point to the set and increases the capacity if necessary
     public void add(Point p) {
 
     }
 
-    // returns the i-th point of the set
+    // TODO returns the i-th point of the set
     public Point get(int i) {
         return null;
     }
 
-    // unifies two sets of points and returns a new set of points
+    // TODO unifies two sets of points and returns a new set of points
     public PointSet unify(PointSet other) {
         return null;
     }
 
-    // constructs a string in this format: [(x1,y1), ..., (xn,yn)]
+    // TODO constructs a string in this format: [(x1,y1), ..., (xn,yn)]
     public String toString() {
         return "test";
     }
 
-    // calculates the convex hull of the set of points
+    // TODO calculates the convex hull of the set of points
     public ConvexPolygon convexHull() {
         return null;
     }
