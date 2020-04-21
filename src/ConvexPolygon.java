c53f35945cc932469e4875250f38f96175c09ece
Johannes Hüsers <S1710307067@students.fh-hagenberg.at>
2020-04-21 21:18:52 +0200

    Implement containsPoint() and ConvexPolygon constructors

--- src/ConvexPolygon.java
+++ src/ConvexPolygon.java
@@ -1,7 +1,36 @@
 public class ConvexPolygon extends PointSet {
 
-    // TODO checks if point p is inside of the convex polygon
+    // constructs a void set of points with capacity 0
+    public ConvexPolygon() {
+        super(0);
+    }
+
+    // constructs a void set of points with the given capacity
+    public ConvexPolygon(int capacity) {
+        super(capacity);
+    }
+
+    // constructs a set of points with the corner points of a convex polygon
+    public ConvexPolygon(ConvexPolygon points) {
+        super(points);
+    }
+
+    // checks if point p is inside of the convex polygon
     public boolean containsPoint(Point p) {
-        return false;
+        Point start;
+        Point end;
+        for (int i = 0; i < size - 1; ++i) {
+            start = buffer[i];
+            end = buffer[i + 1];
+            if (isLeft(start, end, p)) {
+                return false;
+            }
+        }
+        start = buffer[size - 1];
+        end = buffer[0];
+        if (isLeft(start, end, p)) {
+            return false;
+        }
+        return true;
     }
 }
--- src/TestConvexHull.java
+++ src/TestConvexHull.java
@@ -20,15 +20,17 @@ public class TestConvexHull {
 
         ConvexPolygon poly1 = new ConvexPolygon();
         System.out.println(poly1.toString());
-        System.out.println("-------test isLeft()------------");
 
         isLeftTestGreater();
         isLeftTestLess();
         isLeftTestGreaterColinear();
         isLeftTestLessColinear();
+        containsPointTestIn();
+        containsPointTestOut();
     }
 
     private static void isLeftTestGreater() {
+        System.out.println("---------isLeftTestGreater()------------");
         Point start = new Point(15, 55);
         Point end = new Point(65, 75);
         Point test = new Point(30, 90);
@@ -36,6 +38,7 @@ public class TestConvexHull {
     }
 
     private static void isLeftTestLess() {
+        System.out.println("---------isLeftTestLess()------------");
         Point start = new Point(65, 75);
         Point end = new Point(30, 90);
         Point test = new Point(75, 85);
@@ -43,6 +46,7 @@ public class TestConvexHull {
     }
 
     private static void isLeftTestGreaterColinear() {
+        System.out.println("---------isLeftTestGreaterColinear()------------");
         Point start = new Point(90, 92.5);
         Point end = new Point(45, 70);
         Point test = new Point(15, 55);
@@ -50,9 +54,40 @@ public class TestConvexHull {
     }
 
     private static void isLeftTestLessColinear() {
+        System.out.println("---------isLeftTestLessColinear()------------");
         Point start = new Point(45, 70);
         Point end = new Point(90, 92.5);
         Point test = new Point(75, 85);
         System.out.println(PointSet.isLeft(start, end, test));
+    }
+
+    private static void containsPointTestIn() {
+        System.out.println("---------containsPointTestIn()------------");
+        Point p1 = new Point(15, 55);
+        Point p2 = new Point(30, 90);
+        Point p3 = new Point(90, 92.5);
+        Point p4 = new Point(65, 75);
+        Point test = new Point(45, 70);
+        ConvexPolygon poly = new ConvexPolygon();
+        poly.add(p1);
+        poly.add(p2);
+        poly.add(p3);
+        poly.add(p4);
+        System.out.println(poly.containsPoint(test));
+    }
+
+    private static void containsPointTestOut() {
+        System.out.println("---------containsPointTestOut()------------");
+        Point p1 = new Point(15, 55);
+        Point p2 = new Point(30, 90);
+        Point p3 = new Point(90, 92.5);
+        Point p4 = new Point(65, 75);
+        Point test = new Point(55, 60);
+        ConvexPolygon poly = new ConvexPolygon();
+        poly.add(p1);
+        poly.add(p2);
+        poly.add(p3);
+        poly.add(p4);
+        System.out.println(poly.containsPoint(test));
     }
 }
